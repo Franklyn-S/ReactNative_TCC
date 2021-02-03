4a4434abe5357716476cf6dc426503f6a8140a53
Franklyn Seabra <franklyn.seabra@osf.digital>
2021-02-03 14:17:41 -0300

    Tela de corrida mockada

--- src/screens/RunScreen.js
+++ src/screens/RunScreen.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import RNLocation from 'react-native-location';
+
 import Clock from '../assets/clock.svg';
 import Fire from '../assets/fire.svg';
 import Heart from '../assets/valentines-heart.svg';
@@ -7,9 +10,56 @@ import Pause from '../assets/pause.svg';
 import Play from '../assets/play.svg';
 import Stop from '../assets/stop.svg';
 
+const STATES = {
+  stopped: 'stopped',
+  running: 'running',
+  paused: 'paused',
+};
 const RunScreen = () => {
-  // const [distance, setDistance] = useState(0);
-  const [isRunning, setIsRunning] = useState(false);
+  const [currentState, setCurrentState] = useState(STATES.stopped);
+  const [isPermissionGranted, setIsPermissionGranted] = useState(false);
+  const [timer, setTimer] = useState(null);
+  const [hour, setHour] = useState(0);
+  const [min, setMin] = useState(0);
+  const [sec, setSec] = useState(0);
+  const [kmCounter, setKmCounter] = useState(null);
+  const [km, setKm] = useState(0);
+  const [kcalCounter, setKcalCounter] = useState(null);
+  const [kcal, setKcal] = useState(0);
+
+  useEffect(() => {
+    const configureLocation = async () => {
+      RNLocation.configure({
+        distanceFilter: 100,
+        desiredAccuracy: {
+          ios: 'best',
+          android: 'highAccuracy',
+        },
+        interval: 1000,
+        maxWaitTime: 1000,
+      });
+      setIsPermissionGranted(await requestPermission());
+    };
+    configureLocation();
+  }, [setIsPermissionGranted]);
+
+  const requestPermission = async () => {
+    const granted = await RNLocation.requestPermission({
+      ios: 'whenInUse',
+      android: {
+        detail: 'fine',
+        rationale: {
+          title: 'Permissão de localização',
+          message:
+            'Esse aplicativo precisa de permissão para a sua localização ' +
+            'para que possa registrar suas corridas.',
+          buttonNegative: 'Cancelar',
+          buttonPositive: 'OK',
+        },
+      },
+    });
+    return granted;
+  };
   const {
     containerStyle,
     titleStyle,
@@ -22,34 +72,109 @@ const RunScreen = () => {
     buttonsStyle,
     buttonsStyleRunning,
   } = styles;
+  const resetCounters = () => {
+    setHour(0);
+    setMin(0);
+    setSec(0);
+    setKm(0);
+    setKcal(0);
+    setKmCounter(null);
+    setKcalCounter(null);
+    setTimer(null);
+  };
+
+  const startCronometro = () => {
+    let newSec = sec;
+    let newMin = min;
+    const contador = setInterval(() => {
+      newSec = newSec + 1;
+      setSec(s => s + 1);
+      if (newSec === 60) {
+        newSec = 0;
+        setSec(0);
+        newMin = newMin + 1;
+        setMin(m => m + 1);
+        if (newMin === 60) {
+          newMin = 0;
+          setMin(0);
+          setHour(h => h + 1);
+        }
+      }
+    }, 1000);
+    setTimer(contador);
+  };
+
+  const startKm = () => {
+    setKmCounter(
+      setInterval(() => {
+        setKm(k => k + 0.1);
+      }, 5000)
+    );
+  };
+
+  const startKcal = () => {
+    setKcalCounter(
+      setInterval(() => {
+        setKcal(kc => kc + 1);
+      }, 2000)
+    );
+  };
+
+  const startRunning = () => {
+    if (isPermissionGranted) {
+      setCurrentState(STATES.running);
+      startCronometro();
+      startKm();
+      startKcal();
+    }
+  };
+  const pauseRunning = () => {
+    setCurrentState(STATES.paused);
+    clearInterval(timer);
+    clearInterval(kmCounter);
+    clearInterval(kcalCounter);
+  };
+  const stopRunning = () => {
+    setCurrentState(STATES.stopped);
+    resetCounters();
+  };
   return (
     <View style={containerStyle}>
       <Text style={titleStyle}>Corrida</Text>
       <Text style={distanceStyle}>
-        0.0 <Text style={kmStyle}>km</Text>
+        {km.toFixed(1)} <Text style={kmStyle}>km</Text>
       </Text>
       <View style={infosStyle}>
         <View style={infoContainer}>
           <Clock style={infosImgStyle} height={30} width={30} />
-          <Text style={infosTextStyle}>00:00</Text>
+          <Text style={infosTextStyle}>
+            {hour ? `${('00' + hour).slice(-2)}:` : ''}
+            {('00' + min).slice(-2)}:{('00' + sec).slice(-2)}
+          </Text>
         </View>
         <View style={infoContainer}>
           <Fire style={infosImgStyle} height={30} width={30} />
-          <Text style={infosTextStyle}>0 kcal</Text>
+          <Text style={infosTextStyle}>{kcal} kcal</Text>
         </View>
         <View style={infoContainer}>
           <Heart style={infosImgStyle} height={30} width={30} />
           <Text style={infosTextStyle}>-</Text>
         </View>
       </View>
-      {isRunning ? (
+      {currentState === STATES.running ? (
         <View style={buttonsStyleRunning}>
-          <Pause height={130} width={130} />
+          <TouchableOpacity onPress={pauseRunning}>
+            <Pause height={130} width={130} />
+          </TouchableOpacity>
         </View>
       ) : (
         <View style={buttonsStyle}>
-          <Play height={130} width={130} />
-          <Stop height={130} width={130} />
+          <TouchableOpacity onPress={startRunning}>
+            <Play height={130} width={130} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={stopRunning}>
+            <Stop height={130} width={130} />
+          </TouchableOpacity>
         </View>
       )}
     </View>
